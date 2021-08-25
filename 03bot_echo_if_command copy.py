5f2a17fab11e7bce6e6b2d8a13167cc8b8078d75
mikh-maksi <newlevelcenter@gmail.com>
2021-08-26 01:11:27 +0300

    Update 03bot_echo_if_command copy.py

--- 03bot_echo_if_command copy.py
+++ 03bot_echo_if_command copy.py
@@ -3,12 +3,12 @@ from telegram.ext import Updater, MessageHandler, Filters
 def echo(update, context):
     string = update.message.text
     print(string[0])
-    if  '/' in string[0] : 
+    if  string[0] == '/': 
         chat = update.effective_chat
         context.bot.send_message(chat_id=chat.id, text="This is command")
     update.message.reply_text(update.message.text)
 
-updater = Updater("")
+updater = Updater("1958845613:AAF48B3sKwrn-ggwr8LxGdYiygpyePLBs9I")
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(MessageHandler(Filters.all, echo))

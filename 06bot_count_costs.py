3c3bfa1b2b3d7de0c487b09ca05c9e4a6a4ae88e
Mikhail <newlevelcenter@gmail.com>
2021-08-26 02:11:16 +0300

    cost

--- 06bot_count_costs.py
+++ 06bot_count_costs.py
@@ -1,31 +1,26 @@
 from telegram.ext import Updater, MessageHandler, Filters, CommandHandler
 
-n_costs = 0 
-
 def start(update, context):
     chat = update.effective_chat
     context.bot.send_message(chat_id=chat.id, text="Hello! This home finance bot.")
 
 def costs(update, context):
-    # update.message.reply_text(update.message.text)
-    global n_costs
 
     string = update.message.text
-    costs = string.split(' ')
-    n_costs = n_costs + int(costs[1])
+    elements = string.split(' ')
     
     chat = update.effective_chat
-    context.bot.send_message(chat_id=chat.id, text="Your costs is "+str(n_costs))
+    context.bot.send_message(chat_id=chat.id, text="Your cost is "+elements[1])
 
 
 def echo(update, context):
     update.message.reply_text(update.message.text)
 
-updater = Updater("1369610345:AAHq-1NZ7ytIj08OfUTBmToedNPoh_AHiIA")
+updater = Updater("1958845613:AAF48B3sKwrn-ggwr8LxGdYiygpyePLBs9I")
 dispatcher = updater.dispatcher
 
 dispatcher.add_handler(CommandHandler("start", start))
-dispatcher.add_handler(CommandHandler("costs", costs))
+dispatcher.add_handler(CommandHandler("eat", costs))
 dispatcher.add_handler(MessageHandler(Filters.all, echo))
 
 updater.start_polling()
